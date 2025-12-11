cc9e770f30484ebb9223af22d3fda58be4fcd2d4
Jose Montero <jose142578@gmail.com>
2025-12-11 01:52:28 -0400

    chore:removing authorization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-import { auth } from "~/server/auth";
-import { api } from "~/trpc/server";
-import ButtonMoveForm from "./components/commonComponents/button/button-move-form";
-import ButtonMoveReport from "./components/commonComponents/button/button-move-report";
-import AccessDenied from "./components/AccessDenied/accesDenied";
-import ControlPanel from "./components/control-panel";
+
 import InvestigationArticlePage from "./components/search-article/page";
 
 export default async function Home() {
-  const session = await auth();
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
-  if (!session?.user) {
-    return (
-      <>
-        <AccessDenied />
-      </>
-    );
-  }
 
   return <InvestigationArticlePage />;
 }
